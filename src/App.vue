9e8f0faec7bea0a136644a9ffdac3e0ff1debf3e
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2024-01-08 23:18:47 +0800

    Setup vue project with tailwind

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="container mx-auto mt-60 max-w-md">
+        <h1>Hello, World!</h1>
+    </div>
+</template>
+
+<style scoped></style>
