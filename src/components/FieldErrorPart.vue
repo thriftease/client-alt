07b9a2ad8f8977ac6da87020b5e0d5e314f0766e
noamolat-fs <noamolat@fullscale.io>
2024-01-10 17:37:57 +0800

    Add field error components

---
+++ src/components/FieldErrorPart.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div v-bind="$attrs">
+        <slot></slot>
+    </div>
+</template>
---
+++ src/components/FieldErrorsPart.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import FieldErrorPart from "./FieldErrorPart.vue";
+
+defineProps({
+    errors: {
+        type: [Array<string>, String],
+        required: false
+    },
+    hide: {
+        type: Boolean,
+        required: false
+    }
+});
+</script>
+
+<template>
+    <ul
+        v-if="errors instanceof Array && ((!hide && errors.length) || true)"
+        v-bind="$attrs"
+    >
+        <li v-for="(error, i) in errors" :key="i">
+            <FieldErrorPart>{{ error }}</FieldErrorPart>
+        </li>
+    </ul>
+    <FieldErrorPart
+        v-else-if="typeof errors === 'string' && ((!hide && errors) || true)"
+        v-bind="$attrs"
+    >
+        {{ errors }}
+    </FieldErrorPart>
+</template>
