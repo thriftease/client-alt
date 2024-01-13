571b8eefcaf7d985cf4e2718510f776a13968766
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2024-01-13 12:03:55 +0800

    Fix wrong context setup for i18nLink

--- src/utils/apolloClient/i18nLink.ts
+++ src/utils/apolloClient/i18nLink.ts
@@ -3,11 +3,14 @@ import { ApolloLink } from "@apollo/client/core";
 
 const i18nLink = new ApolloLink((operation, forward) => {
     // Modify headers as per your requirement
-    const headers = operation.getContext().headers || {};
+    const context = operation.getContext();
 
     operation.setContext({
-        ...headers,
-        "Accept-Language": i18nClient.global.locale
+        ...context,
+        headers: {
+            ...context.headers,
+            "Accept-Language": i18nClient.global.locale
+        }
     });
     return forward(operation);
 });
