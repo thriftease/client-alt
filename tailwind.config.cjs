fa5128ad89e43b3dbe926bd91efb3f8acd1d774a
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2024-01-14 17:54:54 +0800

    Remove tailwind styles for now

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,5 +5,8 @@ module.exports = {
     theme: {
         extend: {}
     },
-    plugins: [require("@tailwindcss/forms")]
+    // plugins: [require("@tailwindcss/forms")],
+    corePlugins: {
+        preflight: false
+    }
 };
