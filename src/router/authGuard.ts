0ee9ce0e674d0751b96c6cea83ca1714f067a633
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2024-01-14 20:45:22 +0800

    Remove skipAuthCheck for now since the guard is not working on link navigation spam

--- src/router/authGuard.ts
+++ src/router/authGuard.ts
@@ -1,8 +1,7 @@
 import { useAuthStore } from "@/stores";
 import type { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 
-let skipAuthCheck = false;
-const authedRedirection = undefined;
+const authedRedirection = { name: "dashboard" };
 const notAuthedRedirection = { name: "auth-sign-in" };
 
 async function authGuard(
@@ -13,27 +12,20 @@ async function authGuard(
     const authStore = useAuthStore();
     if (!authedRedirection && !notAuthedRedirection) return next();
 
-    if (skipAuthCheck) {
-        skipAuthCheck = false;
-        return next();
-    }
-
     await authStore.verify();
 
     // skipAuthCheck if matches and just proceed with the route
     // since we don't want to re-run this route guard again
     if (to.matched.some((record) => record.meta.authed === true)) {
         if (!authStore.signedIn) {
-            skipAuthCheck = true;
             return next(notAuthedRedirection);
         }
     } else if (to.matched.some((record) => record.meta.authed === false)) {
         if (authStore.signedIn) {
-            skipAuthCheck = true;
             return next(authedRedirection);
         }
     }
-    next();
+    return next();
 }
 
 export default authGuard;
