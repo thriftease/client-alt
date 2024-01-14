dc650e0021a6cfa777f3a1351c4091f9ff5bdd32
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2024-01-14 17:19:33 +0800

    Add null error to utils.handleError and add helper apollo run functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,12 @@ import i18nClient from "@/utils/i18nClient";
 import validators from "@/utils/validators";
 import type { ErrorObject } from "@vuelidate/core";
 
-import { ApolloError } from "@apollo/client/core";
+import {
+    ApolloError,
+    type DocumentNode,
+    type OperationVariables
+} from "@apollo/client/core";
+import { ref } from "vue";
 
 function flattenErrors(errors: ErrorObject[]) {
     return errors.map((e) => e.$message as string);
@@ -13,11 +18,11 @@ function flattenErrors(errors: ErrorObject[]) {
 function handleError<T>(
     response: {
         data?: T;
-        error?: true | ApolloError | ErrorType[] | ErrorType;
+        error?: true | ApolloError | ErrorType[] | ErrorType | null;
     } = {}
 ) {
     const { data, error } = response;
-    if (typeof error === "undefined") return data;
+    if (error === undefined || error === null) return data;
     if (error instanceof ApolloError) {
         alert(error.message);
     } else if (error instanceof Array) {
@@ -37,4 +42,58 @@ function handleError<T>(
     }
 }
 
-export { apolloClient, flattenErrors, handleError, i18nClient, validators };
+async function apolloRun<
+    TInput extends OperationVariables,
+    TPayload extends { [key: string]: any }
+>(type: "mutation" | "query", input: TInput, document: DocumentNode) {
+    const rv = {
+        data: ref<TPayload>(),
+        error: ref<ApolloError>(),
+        loading: ref(false)
+    };
+    rv.loading.value = true;
+    try {
+        if (type === "mutation") {
+            const result = await apolloClient.mutate<TPayload>({
+                mutation: document,
+                variables: input
+            });
+            rv.data.value = result.data!;
+        } else {
+            const result = await apolloClient.query<TPayload>({
+                query: document,
+                variables: input
+            });
+            rv.data.value = result.data!;
+        }
+    } catch (err: any) {
+        rv.error.value = err as ApolloError;
+    }
+    rv.loading.value = false;
+    return rv;
+}
+
+async function apolloQuery<
+    TInput extends OperationVariables,
+    TPayload extends { [key: string]: any }
+>(input: TInput, document: DocumentNode) {
+    return apolloRun<TInput, TPayload>("query", input, document);
+}
+
+async function apolloMutate<
+    TInput extends OperationVariables,
+    TPayload extends { [key: string]: any }
+>(input: TInput, document: DocumentNode) {
+    return apolloRun<TInput, TPayload>("mutation", input, document);
+}
+
+export {
+    apolloClient,
+    apolloMutate,
+    apolloQuery,
+    apolloRun,
+    flattenErrors,
+    handleError,
+    i18nClient,
+    validators
+};
