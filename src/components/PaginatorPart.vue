d8e58b2f793a77706b2cdf18c35f0b24c18b0935
noamolat-fs <noamolat@fullscale.io>
2024-01-15 15:32:39 +0800

    Add perPage input to paginator component

--- src/components/PaginatorPart.vue
+++ src/components/PaginatorPart.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import type { PaginatorQueryInput, PaginatorType } from "@/gql";
 import { computed, ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 const paginatorInput = computed(() => {
     return {
         page: route.query.page ? +route.query.page : 1,
-        perPage: route.query.perPage ? +route.query.perPage : 10
+        perPage: route.query.perPage ? +route.query.perPage || 1 : 10
     } as PaginatorQueryInput;
 });
 const paginator = ref<PaginatorType>();
@@ -16,10 +17,40 @@ defineExpose({ input: paginatorInput, type: paginator });
 const $emits = defineEmits(["input"]);
 
 watch(paginatorInput, () => $emits("input"));
+
+const _perPage = ref(paginatorInput.value.perPage!);
+const perPage = computed({
+    get() {
+        return _perPage.value;
+    },
+    set(v) {
+        _perPage.value = Math.max(v, 1);
+    }
+});
+
+function setPerPage() {
+    router.push({
+        ...route,
+        query: {
+            ...route.query,
+            page: paginator.value!.page.current,
+            perPage: perPage.value
+        },
+        force: true
+    });
+}
 </script>
 
 <template>
     <div v-if="paginator">
+        <label for="perPage">{{ $t("itemsPerPage") }}</label>
+        <input
+            id="perPage"
+            type="number"
+            min="1"
+            @keyup.enter="setPerPage"
+            v-model="perPage"
+        />
         <router-link
             v-if="paginator.page.previous"
             :to="{
