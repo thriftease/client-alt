1815f3cd05db0c2ce5b0e1c1ce206a31fce20a80
noamolat-fs <noamolat@fullscale.io>
2024-01-17 21:49:46 +0800

    Add transactionStore to barrel file

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,11 @@
 import useAccountStore from "@/stores/accountStore";
 import useAuthStore from "@/stores/authStore";
 import useCurrencyStore from "@/stores/currencyStore";
+import useTransactionStore from "@/stores/transactionStore";
 
-export { useAccountStore, useAuthStore, useCurrencyStore };
+export {
+    useAccountStore,
+    useAuthStore,
+    useCurrencyStore,
+    useTransactionStore
+};
