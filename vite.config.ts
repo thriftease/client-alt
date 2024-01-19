85714fb36cee985e2fa312b4361ba3f0be87592b
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2024-01-19 08:08:03 +0800

    Remove 3000 port from vite (accidentally pushed!)

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
     return {
-        server: { port: 3000 },
+        // server: { port: 3000 },
         plugins: [vue()],
         resolve: {
             alias: {
